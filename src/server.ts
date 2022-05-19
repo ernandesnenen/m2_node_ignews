843618e9a1ba9825a3152561afdbe7e2a854603a
ernandes <nenenernan@gmail.com>
2022-05-19 14:59:33 -0300

    docker-compose

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
 import { routers } from './Routes';
+
+import swaggerFile from './swagger.json'
 
 
 
 const app = express();
 
 app.use(express.json())
+
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use(routers)
 
